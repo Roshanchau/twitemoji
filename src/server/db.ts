8550eae403697fdb8f14b90618b32191ae04df0b
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2023-12-27 22:32:29 +0545

    post router get query added emoji

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,13 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env";
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const db =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
