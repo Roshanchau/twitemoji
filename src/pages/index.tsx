7c2e01661b5acc99e55ae4ce485e74c4a545e0aa
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2023-12-29 23:25:00 +0545

    creating posts and connecting them to users!!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,22 @@ import Head from "next/head";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
+const CreatePostWizard=()=>{
+  const {user}=useUser();
+  console.log(user);
+  if(!user) return null;
+
+  return(
+    <div className="flex w-full gap-3">
+      <img src={user.profileImageUrl} alt="profile image"
+      className="h-14 w-14 rounded-full"
+      />
+      <input placeholder="Type some emoji's!!"
+      className="bg-transparent grow outline-none"/>
+    </div>
+  )
+}
+
 export default function Home() {
   const user = useUser();
   const { data , isLoading } = api.post.getAll.useQuery();
@@ -29,10 +45,10 @@ export default function Home() {
                 <SignInButton />
               </div>
             )}
-            {!!user.isSignedIn && <SignOutButton />}
+            {!!user.isSignedIn && <CreatePostWizard />}
           </div>
           <div className="flex flex-col">
-            {[...data, ...data].map((post) => <div
+            {[...data, ...data].map(({post ,author}) => <div
              className="border-b border-slate-400 p-8"
             key={post.id}>{post.content}</div>)}
           </div>
