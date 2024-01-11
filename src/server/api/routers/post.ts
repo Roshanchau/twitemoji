cb0d760b668264c5ef2cf098ec7676a7c55246a5
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-01-11 17:26:54 +0545

    Rate limiting with upstash

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -13,6 +13,17 @@ const filterUserForClient=(user:User)=>{
   };
 }
 
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
+
+// Create a new ratelimiter, that allows 3 requests per 1 min.
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
+
 export const postRouter = createTRPCRouter({
 
   getAll: publicProcedure.query(async({ ctx }) => {
@@ -54,6 +65,10 @@ export const postRouter = createTRPCRouter({
     )
     .mutation(async({ctx, input})=>{
       const authorId=ctx.currentUser;
+
+      const {success}=await ratelimit.limit(authorId);
+
+      if(!success) throw new TRPCError({code:"TOO_MANY_REQUESTS"})
 
       const post=await ctx.db.post.create({
         data:{
