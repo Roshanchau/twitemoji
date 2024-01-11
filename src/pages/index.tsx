c0ab35aae2eac8b30155f8d80498ee8e46eab085
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-01-11 18:53:21 +0545

    Routing- Profile view & Post view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import {
 } from "~/components/LoadingSpininer/LoadingSpinner";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -89,10 +90,13 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/@${post.id}`}>
           <span className="font-thin">{`· ${dayjs(
             post.createdAt,
-          ).fromNow()}`}</span>
+          ).fromNow()}`}</span></Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
