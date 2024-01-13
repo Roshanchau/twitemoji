3c70025309f11d3f69dab09dfdc2a83221b86546
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-01-13 21:56:46 +0545

    creating profile router part 1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,18 @@ import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Twitmoji</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,10 @@
+import { clerkClient } from "@clerk/nextjs";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+
+export const profileRouter = createTRPCRouter({
+
+});
