1a44ef630333ef34b51c43519243d0694fa88ed1
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-01-14 13:10:32 +0545

    creating profile Router.

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,14 +1,23 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
 
 const ProfilePage: NextPage = () => {
+  const {data, isLoading}=api.profile.getUserByusername.useQuery({
+    username:"roshanchau",
+  })
+  if(isLoading) return <div>loading....</div>;
+
+  if(!data) return <div>404</div>;
+  console.log(data)
+
   return (
     <>
       <Head>
         <title>Profile</title>
       </Head>
       <main className="flex h-screen justify-center">
-        <div>Profile View</div>
+        <div>{data.username}</div>
       </main>
     </>
   );
--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -3,8 +3,22 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 export const profileRouter = createTRPCRouter({
+  getUserByusername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const [user] = await clerkClient.users.getUserList({
+        username: [input.username],
+      });
 
+      if (!user) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "User not found!!",
+        });
+      }
+      return filterUserForClient(user);
+    }),
 });
